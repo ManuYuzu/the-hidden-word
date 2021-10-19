f189aa9c3727e2fa0331a637384f3b46eba70b51
Manu Glez <manuaglez@gmail.com>
2021-10-19 17:32:33 +0100

    Solutions checked

--- src/__test__/main.test.ts
+++ src/__test__/main.test.ts
@@ -1,14 +1,15 @@
 import { hiddenWord } from '../main'
 
-const chai = require("chai");
-const assert = chai.assert;
-chai.config.truncateThreshold = 0;
+const chai = require('chai')
+const assert = chai.assert
+
+chai.config.truncateThreshold = 0
 
 describe("Tests", () => {
   it("test", () => {
-    assert.strictEqual(hiddenWord(637), "aid");
-    assert.strictEqual(hiddenWord(7415), "debt");
-    assert.strictEqual(hiddenWord(49632), "email");
-    assert.strictEqual(hiddenWord(942547), "melted");
-  });
-});
+    assert.strictEqual(hiddenWord(637), "aid")
+    assert.strictEqual(hiddenWord(7415), "debt")
+    assert.strictEqual(hiddenWord(49632), "email")
+    assert.strictEqual(hiddenWord(942547), "melted")
+  })
+})
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,34 @@
 export function hiddenWord(num:number) {
 
+  // Posibility 1
+  return num
+    .toString()
+    .replace(/6/gi, 'a')
+    .replace(/1/gi, 'b')
+    .replace(/7/gi, 'd')
+    .replace(/4/gi, 'e')
+    .replace(/3/gi, 'i')
+    .replace(/2/gi, 'l')
+    .replace(/9/gi, 'm')
+    .replace(/8/gi, 'n')
+    .replace(/0/gi, 'o')
+    .replace(/5/gi, 't')
+
+    // Posibility 2
+    /*
+    let keys = {
+      '6':'a',
+      '1':'b',
+      '7':'d',
+      '4':'e',
+      '3':'i',
+      '2':'l',
+      '9':'m',
+      '8':'n',
+      '0':'o',
+      '5':'t'
+    }
+
+    return num.toString().split('').map(value => keys[value]).join('')
+    */
 }
