29d7475a96892b572f775c68f0233840480f4def
Manu Glez <manuaglez@gmail.com>
2021-10-19 16:23:21 +0100

    🚀 Testing and project setted up

--- src/__test__/main.test.ts
+++ src/__test__/main.test.ts
@@ -1,5 +1,14 @@
-describe('dummy test', () => {
-  it('should ok', function () {
-    expect(2 + 2).toEqual(4);
+import { hiddenWord } from '../main'
+
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold = 0;
+
+describe("Tests", () => {
+  it("test", () => {
+    assert.strictEqual(hiddenWord(637), "aid");
+    assert.strictEqual(hiddenWord(7415), "debt");
+    assert.strictEqual(hiddenWord(49632), "email");
+    assert.strictEqual(hiddenWord(942547), "melted");
   });
 });
